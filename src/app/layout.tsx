8f6b0b6a0a6ee382c7fe1e43ce79dee431e0d3bb
razoanasatu <razoanasultanasaru1172gmail.com>
2025-05-20 03:58:30 +0600

    fixed_error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import AdminLayout from "../../components/AdminLayout";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="antialiased">
         <AdminLayout>{children}</AdminLayout>
       </body>
