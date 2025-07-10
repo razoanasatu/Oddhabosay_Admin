e538730d240cc4ee5b4f07f19d42929e631e17a9
razoanasatu <razoanasultanasatu117@gmail.com>
2025-07-10 21:54:42 +0600

    config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,17 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Add this line to transpile @react-pdf/renderer and its dependencies
+  transpilePackages: [
+    "@react-pdf/renderer",
+    "@react-pdf/font",
+    "@react-pdf/text",
+    "@react-pdf/view",
+    "@react-pdf/page",
+    "@react-pdf/document",
+    "@react-pdf/image",
+    "@react-pdf/primitives",
+  ],
 };
 
 /* config options here */
